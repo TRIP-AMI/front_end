8d6e59cb1cea349047688b53b9f9754a812427a4
hyobicho <hyobicho@student.42seoul.kr>
2023-11-29 18:29:04 +0900

    [Style] [develop] 기본 color 색상코드 추가

--- styles/colors.ts
+++ styles/colors.ts
@@ -4,7 +4,13 @@
 
 const Colors = {
   main: '#ED6D34',
-  footer: '#404040',
+  titleBlack: '#161616',
+  moreGray: '#77787A',
+  navIcon: '#1E1E20',
+  subtitle: '#4A4A4A',
+  likeBlank: '#CBCBCBE3',
+  footerBackground: '#404040',
+  footerText: '#B9B9B9',
   footerLight: '#808080',
 };
 
