64a9c1d861c9469ba6b2034d7e80e68302f04340
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-12-03 01:42:02 +0900

    [Feat] [FE-10] modal 1차 퍼블리싱 완료

--- src/components/organisms/Modal/SearchModal.tsx
+++ src/components/organisms/Modal/SearchModal.tsx
@@ -5,9 +5,12 @@ import {
   SafeAreaView,
   Text,
   TextInput,
+  StyleSheet,
 } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { useState } from 'react';
+import Spacing from '@styles/spacing';
+import Colors from '@styles/colors';
 
 function SearchModal({
   isVisible,
@@ -17,6 +20,54 @@ function SearchModal({
   onClose: () => void;
 }) {
   const [text, setText] = useState('');
+  const [tagList] = useState([
+    {
+      id: Math.random(),
+      label: '#Itaewon',
+    },
+    {
+      id: Math.random(),
+      label: '#Itaewon',
+    },
+    {
+      id: Math.random(),
+      label: '#Itaewon',
+    },
+    {
+      id: Math.random(),
+      label: '#Itaewon',
+    },
+    {
+      id: Math.random(),
+      label: '#Itaewon',
+    },
+  ]);
+  const [topProgramList] = useState([
+    {
+      id: Math.random(),
+      label: "Jenny's Itaewon tour",
+    },
+    {
+      id: Math.random(),
+      label: "Jenny's Itaewon tour",
+    },
+    {
+      id: Math.random(),
+      label: "Jenny's Itaewon tour",
+    },
+    {
+      id: Math.random(),
+      label: "Jenny's Itaewon tour",
+    },
+    {
+      id: Math.random(),
+      label: "Jenny's Itaewon tour",
+    },
+  ]);
+
+  const onPressTag = () => {};
+  const onPressProgram = () => {};
+
   return (
     <Modal
       animationType='slide'
@@ -29,30 +80,145 @@ function SearchModal({
           style={{
             flexDirection: 'row',
             justifyContent: 'flex-end',
-            padding: 10,
+            paddingVertical: 15,
+            paddingHorizontal: Spacing.IOS392Margin,
           }}
         >
           <Pressable onPress={onClose}>
             <AntDesign name='close' size={24} color='black' />
           </Pressable>
         </View>
-        <View style={{ padding: 10 }}>
+        <View style={{ paddingHorizontal: Spacing.IOS392Margin }}>
           {/* search input */}
-          <View style={{ borderColor: '#000', borderWidth: 2 }}>
+          <View style={style.searchWrap}>
+            <AntDesign name='search1' size={18} color={Colors.main} />
             <TextInput
-              style={{ height: 40 }}
-              placeholder='Type here to translate!'
+              style={style.searchInput}
+              placeholder='What kind of trip do you want?'
               onChangeText={(newText) => setText(newText)}
               defaultValue={text}
             />
           </View>
           {/* recommeded keywords */}
+          <View>
+            <Text style={[style.textTitle, { marginBottom: 20 }]}>
+              Recommended keywords
+            </Text>
+            <View style={style.tagWrap}>
+              {tagList.map((tag) => {
+                return (
+                  <Pressable
+                    key={tag.id}
+                    onPress={onPressTag}
+                    style={style.tag}
+                  >
+                    <Text style={{ color: '#161616' }}>{tag.label}</Text>
+                  </Pressable>
+                );
+              })}
+            </View>
+          </View>
           {/* top 5 program */}
-          <Text>contents</Text>
+          <Text style={[style.textTitle, { marginTop: 30 }]}>
+            Top 5 Program
+          </Text>
+          <Text
+            style={[
+              style.textTitleSub,
+              { marginTop: 6, marginBottom: 20 - 12.5 },
+            ]}
+          >
+            The most requested AMI on the last 3 days.
+          </Text>
+
+          {topProgramList.map((program, i) => {
+            return (
+              <Pressable
+                key={program.id}
+                onPress={onPressProgram}
+                style={style.programWrap}
+              >
+                <Text
+                  style={{
+                    color: Colors.main,
+                    fontSize: 14,
+                    fontFamily: 'Montserrat-ExtraBold',
+                    lineHeight: 18,
+                    letterSpacing: 0.28,
+                  }}
+                >
+                  {i + 1}
+                </Text>
+                <Text
+                  style={{
+                    marginLeft: 8,
+                    color: '#292929',
+                    fontSize: 14,
+                    fontFamily: 'Montserrat-Bold',
+                    lineHeight: 18,
+                    letterSpacing: 0.28,
+                  }}
+                >
+                  {program.label}
+                </Text>
+              </Pressable>
+            );
+          })}
         </View>
       </SafeAreaView>
     </Modal>
   );
 }
+
+const style = StyleSheet.create({
+  searchWrap: {
+    flexDirection: 'row',
+    borderColor: '#ECECEC',
+    borderWidth: 1,
+    borderRadius: 30,
+    marginBottom: 30,
+    paddingVertical: 18,
+    paddingHorizontal: 14,
+    shadowColor: '#0000001C',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 3,
+    backgroundColor: '#fff',
+  },
+  searchInput: {
+    marginLeft: 8,
+  },
+  textTitle: {
+    color: Colors.titleBlack,
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 22,
+    lineHeight: 22,
+    letterSpacing: 0,
+  },
+  tagWrap: {
+    flexDirection: 'row',
+  },
+  tag: {
+    marginRight: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 11,
+    borderColor: '#CCCCCC',
+    borderWidth: 1,
+    borderRadius: 18,
+  },
+  textTitleSub: {
+    color: '#8E8E8E',
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 13,
+    lineHeight: 16,
+  },
+  programWrap: {
+    flexDirection: 'row',
+    paddingVertical: 12.5,
+  },
+});
 
 export default SearchModal;
--- src/styles/spacing.ts
+++ src/styles/spacing.ts
@@ -1,3 +1,9 @@
 /*
   Paddings, margins and scale
  */
+
+const Spacing = {
+  IOS392Margin: 24,
+};
+
+export default Spacing;
