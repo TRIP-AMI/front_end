c278b54f9e3b339f31221e2a3a78a92ded2275cb
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-12-03 21:42:07 +0900

    [Feat] [FE-13] menu screen 아이콘 추가

--- src/navigations/BottomNavBar.tsx
+++ src/navigations/BottomNavBar.tsx
@@ -31,6 +31,7 @@ export default function BottomNavBar() {
           tabBarIcon: ({ color }) => (
             <Ionicons name='menu' color={color} size={26} />
           ),
+          headerRight: () => <HomeHeaderIcons />,
           title: 'MENU',
         }}
       />
