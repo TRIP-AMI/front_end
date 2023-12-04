f6cd4be102b9dc7796d37ad418f9c4486bb45fe8
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-12-04 15:09:31 +0900

    [Refactor] [FE-15] 플랜아이템 리팩토링

--- src/components/molecules/Item/PlanItem.tsx
+++ src/components/molecules/Item/PlanItem.tsx
@@ -1,42 +1,17 @@
-import LottieView from 'lottie-react-native';
-import { useRef } from 'react';
-import { View, ImageBackground, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Plan from '@/types/plan';
 import PlanItemTitle from '@/components/atoms/Text/PlanItemTitle';
 import PlanItemSubTitle from '@/components/atoms/Text/PlanItemSubTitle';
+import PlanItemImg from '@/components/atoms/Image/PlanItemImg';
+import LikeHeart from '@/components/atoms/Lottie/LikeHeart';
 
 export default function PlanItem({ planItem }: { planItem: Plan }) {
-  const likeBtn = useRef<LottieView>(null);
-
-  const likePress = () => {
-    likeBtn.current?.reset();
-    likeBtn.current?.play(0, 25);
-  };
   return (
     <View style={styles.planItemWrap}>
       <View style={styles.imgWrap}>
-        <ImageBackground
-          source={{
-            uri: planItem.planImg,
-          }}
-          resizeMode='cover'
-          style={{ flex: 1 }}
-        >
-          <Pressable
-            onPress={likePress}
-            style={{ position: 'absolute', width: 50, bottom: 0, right: 0 }}
-          >
-            <LottieView
-              ref={likeBtn}
-              // TODO: lint 수정필요
-              // eslint-disable-next-line import/extensions
-              source={require('../../../../assets/lottie/Animation - 1701609917706.json')}
-              style={{ width: '100%', aspectRatio: 1 }}
-              autoPlay={false}
-              loop={false}
-            />
-          </Pressable>
-        </ImageBackground>
+        <PlanItemImg planUri={planItem.planImg}>
+          <LikeHeart />
+        </PlanItemImg>
       </View>
       <View>
         <PlanItemTitle style={{ marginBottom: 1 }}>
@@ -57,5 +32,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 161,
     marginBottom: 8,
+    borderRadius: 5,
+    overflow: 'hidden',
   },
 });
