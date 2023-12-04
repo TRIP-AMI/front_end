a6bbfe8998b47f386b7eb25a18bac78b01b59b91
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-12-04 15:08:24 +0900

    [Feat] [FE-15] lottie 애니메이션 변경 및 컴포넌트 분리

---
+++ src/components/atoms/Lottie/LikeHeart.tsx
@@ -0,0 +1,35 @@
+import LottieView from 'lottie-react-native';
+import { useRef, useState } from 'react';
+import { Pressable } from 'react-native';
+
+export default function LikeHeart() {
+  const likeBtn = useRef<LottieView>(null);
+  const [isOn, setIsOn] = useState(false);
+
+  const likePress = () => {
+    setIsOn((prev) => !prev);
+
+    if (isOn) {
+      likeBtn.current?.reset();
+      likeBtn.current?.play(10, 30);
+    } else {
+      likeBtn.current?.reset();
+    }
+  };
+  return (
+    <Pressable
+      onPress={likePress}
+      style={{ position: 'absolute', width: 50, bottom: 0, right: 0 }}
+    >
+      <LottieView
+        ref={likeBtn}
+        // TODO: lint 수정필요
+        // eslint-disable-next-line import/extensions
+        source={require('../../../../assets/lottie/Animation_backGrayHeart.json')}
+        style={{ width: '100%', aspectRatio: 1 }}
+        autoPlay={false}
+        loop={false}
+      />
+    </Pressable>
+  );
+}
