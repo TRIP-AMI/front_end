b8bac396f13f15bb99a0e8e29e735b1b4ae7b8fa
hyobicho <hyobicho@student.42seoul.kr>
2023-12-07 17:06:31 +0900

    [Test] [FE-19] 버튼 테스트용 코드

--- src/screens/ContentScreen.tsx
+++ src/screens/ContentScreen.tsx
@@ -5,6 +5,11 @@ import ContentAmi from '@components/organisms/Section/ContentAmi';
 import ContentProgram from '@components/organisms/Section/ContentProgram';
 import ContentBannerCarousel from '@components/molecules/Banner/ContentBannerCarousel';
 import ContentPayment from '@components/molecules/Section/ContentPayment';
+import MediumButton from '@/components/atoms/Button/MediumButton';
+import FullSizeButton from '@/components/atoms/Button/FullSizeButton';
+import LargeButton from '@/components/atoms/Button/LargeButton';
+import SmallButton from '@/components/atoms/Button/SmallButton';
+import OutlinedButton from '@/components/atoms/Button/OutlinedButton';
 
 export default function ContentScreen() {
   return (
@@ -16,6 +21,31 @@ export default function ContentScreen() {
         <ContentAmi />
         <ContentProgram />
         <ContentPayment />
+        {/* TODO: 테스트 용 코드, 머지 전 지우기 */}
+        <MediumButton
+          content='Apply'
+          active
+          onPress={() => console.log('medium')}
+        />
+        <SmallButton
+          content='Cancel'
+          active
+          onPress={() => console.log('small')}
+        />
+        <OutlinedButton
+          content='Cancel'
+          onPress={() => console.log('outline')}
+        />
+        <FullSizeButton
+          content='Cancel'
+          active={false}
+          onPress={() => console.log('full size')}
+        />
+        <LargeButton
+          content='Cancel'
+          active
+          onPress={() => console.log('large')}
+        />
       </ScrollView>
     </SafeAreaView>
   );
