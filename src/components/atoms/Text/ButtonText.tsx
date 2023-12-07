616c2df43e3d78896f433ba6ca081465a478ffac
hyobicho <hyobicho@student.42seoul.kr>
2023-12-07 17:04:45 +0900

    [Feat] [FE-19] 버튼 텍스트 아톰

---
+++ src/components/atoms/Text/ButtonText.tsx
@@ -0,0 +1,20 @@
+import { Text, StyleSheet } from 'react-native';
+
+export default function ButtonText({
+  content,
+  color,
+}: {
+  content: string;
+  color: string;
+}) {
+  return <Text style={[styles.text, { color }]}>{content}</Text>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 16,
+    lineHeight: 18,
+    textAlign: 'center',
+  },
+});
