21cac7f2e56f1b184dddb3ed577f499e3606c91d
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-12-07 13:08:28 +0900

    [Fix] [develop] api root url 변경

--- src/services/config/axios.ts
+++ src/services/config/axios.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
+// local mock 서버
+/**
+ * 1. http://<내부ip>:9999
+ *  package.json stdb script 변경
+ * "stdb": "json-server --host <내부ip> --watch db.json -p 9999"
+ * 2. http://localhost:9999
+ * xcode 시뮬레이터 사용시 그냥 npm run stdb 실행
+ */
+// export const BASE_API_URL = 'http://10.19.201.109:9999';
+export const BASE_API_URL = 'http://localhost:9999';
+
+// TODO: 개발서버 url
+// export const BASE_API_URL = 'http://43.202.134.168:8080';
+
 // create an axios instance
 const instance = axios.create({
   baseURL: 'exp://192.168.10.140:8081',
--- src/services/module/plan/plan.ts
+++ src/services/module/plan/plan.ts
@@ -1,7 +1,7 @@
-import instance from '@/services/config/axios';
+import instance, { BASE_API_URL } from '@/services/config/axios';
 
 const getPlanList = () => {
-  const url = `http://10.19.201.109:9999/plan`;
+  const url = `${BASE_API_URL}/plan`;
   const method = 'GET';
   return instance({ url, method });
 };
