a6d49fca2dd19131d9d2d70b1049bf86422c0d60
Myoung Jun Ko <kmj951015@gmail.com>
2023-12-07 11:47:01 +0900

    [Feat] [FE-12] Content Payment 추가

---
+++ src/components/molecules/Section/ContentPayment.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from 'react-native';
+import TextTitle from '@components/atoms/Text/TextTitle';
+import Spacing from '@styles/spacing';
+import Separator from '@components/atoms/etc/Separator';
+import colors from '@styles/colors';
+
+function ContentPayment() {
+  return (
+    <View>
+      <View style={styles.container}>
+        <TextTitle style={styles.title}>Payment</TextTitle>
+        <Text>
+          1. Apply{'\n'}2. Confirmation Notification Email{'\n'}3. Pay directly
+          to AMI the day of the trip
+        </Text>
+      </View>
+      <Separator color={colors.blankMoreGray} hei={8} marginVer={30} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: Spacing.IOS392Margin,
+  },
+  title: {
+    color: '#000000',
+    fontSize: 18,
+    fontFamily: 'Montserrat-Bold',
+    letterSpacing: -0.36,
+    marginBottom: 15,
+  },
+  text: {
+    color: '#000000',
+    fontSize: 16,
+    fontFamily: 'Montserrat-Regular',
+    letterSpacing: -0.32,
+  },
+});
+
+export default ContentPayment;
--- src/screens/ContentScreen.tsx
+++ src/screens/ContentScreen.tsx
@@ -3,15 +3,19 @@ import { StatusBar } from 'expo-status-bar';
 import ContentMain from '@components/organisms/Section/ContentMain';
 import ContentAmi from '@components/organisms/Section/ContentAmi';
 import ContentProgram from '@components/organisms/Section/ContentProgram';
+import ContentBannerCarousel from '@components/molecules/Banner/ContentBannerCarousel';
+import ContentPayment from '@components/molecules/Section/ContentPayment';
 
 export default function ContentScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style='auto' />
       <ScrollView showsVerticalScrollIndicator={false}>
+        <ContentBannerCarousel />
         <ContentMain />
         <ContentAmi />
         <ContentProgram />
+        <ContentPayment />
       </ScrollView>
     </SafeAreaView>
   );
@@ -21,7 +25,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
