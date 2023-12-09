dcade6473b89217ef0f22d1fc45ee477826b1616
hyobicho <hyobicho@student.42seoul.kr>
2023-12-09 15:39:05 +0900

    [Feat] [FE-19] 아웃라인 버튼 컴포넌트

--- src/components/atoms/Button/OutlinedButton.tsx
+++ src/components/atoms/Button/OutlinedButton.tsx
@@ -1,28 +1,40 @@
 import { Pressable, StyleSheet } from 'react-native';
-import ButtonText from '@components/atoms/Text/ButtonText';
+import ButtonText from '@/components/atoms/Text/ButtonText';
 import Colors from '@/styles/colors';
+
+type OutlinedButtonProps = {
+  content: string;
+  // eslint-disable-next-line react/require-default-props
+  disabled?: boolean;
+  onPress: () => void;
+};
 
 export default function OutlinedButton({
   content,
+  disabled,
   onPress,
-}: {
-  content: string;
-  onPress: () => void;
-}) {
+}: OutlinedButtonProps) {
   return (
     <Pressable
-      style={({ pressed }) => [pressed && styles.pressed, styles.container]}
+      style={({ pressed }) => [
+        styles.container,
+        pressed && styles.pressed,
+        disabled && styles.disabled,
+      ]}
       onPress={onPress}
     >
-      <ButtonText content={content} color={Colors.main} />
+      <ButtonText
+        content={content}
+        color={disabled ? Colors.contentLine : Colors.main}
+      />
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: 345,
-    height: 42,
+    width: '100%',
+    height: '100%',
     backgroundColor: '#ffffff',
     borderWidth: 1,
     borderColor: Colors.main,
@@ -31,5 +43,8 @@ const styles = StyleSheet.create({
   },
   pressed: {
     opacity: 0.5,
+  },
+  disabled: {
+    borderColor: Colors.borderGray,
   },
 });
