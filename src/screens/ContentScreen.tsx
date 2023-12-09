2bab5476ac45ceb542397c28582e5e17102a5177
hyobicho <hyobicho@student.42seoul.kr>
2023-12-10 04:03:12 +0900

    [Test] [FE-26] 로그인 이동 모달 테스트 코드

--- src/screens/ContentScreen.tsx
+++ src/screens/ContentScreen.tsx
@@ -1,12 +1,22 @@
-import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import ContentMain from '@components/organisms/Section/ContentMain';
 import ContentAmi from '@components/organisms/Section/ContentAmi';
 import ContentProgram from '@components/organisms/Section/ContentProgram';
 import ContentBannerCarousel from '@components/molecules/Banner/ContentBannerCarousel';
 import ContentPayment from '@components/molecules/Section/ContentPayment';
+import BasicButton from '@components/atoms/Button/BasicButton';
+import { useSetRecoilState } from 'recoil';
+import modalState from '@/utils/recoil/modal';
 
 export default function ContentScreen() {
+  const setModal = useSetRecoilState(modalState);
+
+  // TODO: SelectOptions modal로 수정할 예정
+  const onApply = () => {
+    setModal({ modalName: 'LOGIN_CHECK' });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style='auto' />
@@ -17,6 +27,10 @@ export default function ContentScreen() {
         <ContentProgram />
         <ContentPayment />
       </ScrollView>
+      {/* 테스트용 코드 */}
+      <View style={{ height: 50, marginVertical: 5 }}>
+        <BasicButton content='Apply' onPress={onApply} />
+      </View>
     </SafeAreaView>
   );
 }
