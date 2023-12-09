32d1db3df0a1575ce3c4ad1bb5043cc5e6beec0b
Myoung Jun Ko <kmj951015@gmail.com>
2023-12-10 00:48:34 +0900

    [Chore] [FE-16] 주석 설명 추가

--- src/screens/MyPageScreen.tsx
+++ src/screens/MyPageScreen.tsx
@@ -53,7 +53,7 @@ export default function MyPageScreen() {
       <MyTab />
       {/* TODO 전체 화면 스크롤을 위해선 Flatlist로 해야하지만
       그러면 TabView와 충돌이 일어나서 TabView 내부 Flatlist가 스크롤되지 않음(같은 방향의 스크롤에서 문제가 생긴다고 함)
-      TabView와 충돌을 피하기 위해 내부 아이템만 스크롤되게 만들면 Footer를 넣기가 애매해짐 */}
+      TabView와 충돌을 피하기 위해 내부 아이템만 스크롤되게 만들면(현재 상태) Footer를 넣기가 애매해짐 */}
       {/* <FlatList */}
       {/*  data={[]} */}
       {/*  renderItem={null} */}
