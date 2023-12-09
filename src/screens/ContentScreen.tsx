83279ded293e399a0e1ccb0e4fa7cff30207d6bd
hyobicho <hyobicho@student.42seoul.kr>
2023-12-09 16:55:38 +0900

    [Chore] [FE-19] 테스트용 코드 삭제

--- src/screens/ContentScreen.tsx
+++ src/screens/ContentScreen.tsx
@@ -1,13 +1,10 @@
-import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import ContentMain from '@components/organisms/Section/ContentMain';
 import ContentAmi from '@components/organisms/Section/ContentAmi';
 import ContentProgram from '@components/organisms/Section/ContentProgram';
 import ContentBannerCarousel from '@components/molecules/Banner/ContentBannerCarousel';
 import ContentPayment from '@components/molecules/Section/ContentPayment';
-import BasicButton from '@/components/atoms/Button/BasicButton';
-import OutlinedButton from '@/components/atoms/Button/OutlinedButton';
-import CancelButton from '@/components/atoms/Button/CancelButton';
 
 export default function ContentScreen() {
   return (
@@ -20,40 +17,6 @@ export default function ContentScreen() {
         <ContentProgram />
         <ContentPayment />
       </ScrollView>
-      {/* 테스트용 코드 */}
-      <View style={{ height: 50, marginVertical: 5 }}>
-        <BasicButton content='Apply' onPress={() => {}} />
-      </View>
-      <View
-        style={{
-          width: '88%',
-          alignSelf: 'center',
-          height: 50,
-          marginVertical: 5,
-        }}
-      >
-        <BasicButton content='Apply' round disabled onPress={() => {}} />
-      </View>
-      <View
-        style={{
-          width: '88%',
-          height: 42,
-          alignSelf: 'center',
-          marginVertical: 5,
-        }}
-      >
-        <OutlinedButton content='Apply' onPress={() => {}} />
-      </View>
-      <View
-        style={{
-          width: '50%',
-          height: 50,
-          alignSelf: 'center',
-          marginVertical: 5,
-        }}
-      >
-        <CancelButton content='Apply' onPress={() => {}} />
-      </View>
     </SafeAreaView>
   );
 }
