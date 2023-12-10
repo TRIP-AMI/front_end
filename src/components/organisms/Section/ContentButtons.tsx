11d5958825f4a7dafd8d34ed389b9e7aaede4380
hyobicho <hyobicho@student.42seoul.kr>
2023-12-10 16:36:15 +0900

    [Feat] [FE-20] 프로그램 하단 버튼 퍼블리싱

--- src/components/organisms/Section/ContentButtons.tsx
+++ src/components/organisms/Section/ContentButtons.tsx
@@ -1,12 +1,25 @@
+import { useSetRecoilState } from 'recoil';
 import { View, StyleSheet } from 'react-native';
 import LikeButtonBox from '@/components/molecules/etc/LikeButtonBox';
-import StyledButton from '@/components/atoms/Button/StyledButton';
+import BasicButton from '@/components/atoms/Button/BasicButton';
+import modalState from '@/utils/recoil/modal';
+import Spacing from '@/styles/spacing';
 
+// TODO: disabled 상태 확인 필요
 export default function ContentButtons() {
+  const setModal = useSetRecoilState(modalState);
+
+  // TODO: SelectOptions modal로 수정할 예정
+  const onApply = () => {
+    setModal({ modalName: 'LOGIN_CHECK' });
+  };
+
   return (
     <View style={styles.container}>
       <LikeButtonBox />
-      <StyledButton type='medium' content='Apply' active onPress={() => {}} />
+      <View style={styles.buttonContainer}>
+        <BasicButton content='Apply' round onPress={onApply} />
+      </View>
     </View>
   );
 }
@@ -15,9 +28,14 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
     width: '100%',
     height: 66,
-    paddingVertical: 8,
-    paddingHorizontal: 24,
+    paddingHorizontal: Spacing.IOS392Margin,
+    backgroundColor: '#ffffff',
+  },
+  buttonContainer: {
+    width: '80%',
+    height: 50,
   },
 });
