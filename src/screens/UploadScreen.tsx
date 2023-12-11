5fea8b1a820dc47b786c18b51c9e04e131a12451
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-12-11 21:48:56 +0900

    [Fix] [FE-24] 임시 open 기능

--- src/screens/UploadScreen.tsx
+++ src/screens/UploadScreen.tsx
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { useSetRecoilState } from 'recoil';
 import Spacing from '@/styles/spacing';
+import modalState from '@/utils/recoil/modal';
 
 export default function UploadScreen() {
+  const setModal = useSetRecoilState(modalState);
+
+  const handleClick = () => {
+    setModal({ modalName: 'CALENDAR' });
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
       <View>
         <Text>temp</Text>
+        <Button title='click' onPress={handleClick} />
       </View>
     </View>
   );
