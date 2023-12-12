712a7111bdc7a0d8e85e3971dffbd7af4422c872
hyobicho <hyobicho@student.42seoul.kr>
2023-12-12 14:20:36 +0900

    [Style] [FE-22] 기본 모달 텍스트 정렬 수정

--- src/components/atoms/Text/BasicModalText.tsx
+++ src/components/atoms/Text/BasicModalText.tsx
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Montserrat-Medium',
     fontSize: 14,
     lineHeight: 20,
-    textAlign: 'center',
+    textAlign: 'left',
     color: Colors.fontGray01,
   },
 });
