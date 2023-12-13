1d426d7894bac7c768b0864266dc31aece516668
Myoung Jun Ko <kmj951015@gmail.com>
2023-12-14 02:49:50 +0900

    [Feat] [FE-16] Application Detail 탭 및 헤더 설정

---
+++ src/screens/ApplicationDetailsScreen.tsx
@@ -0,0 +1,32 @@
+import { SafeAreaView, StyleSheet } from 'react-native';
+import BasicTab from '@components/organisms/Section/BasicTab';
+
+function ApplicationDetailsScreen() {
+  const tabData = [
+    {
+      id: 1,
+      tabName: 'Application',
+      tabComponent: null,
+    },
+    {
+      id: 2,
+      tabName: 'Cancellation',
+      tabComponent: null,
+    },
+  ];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <BasicTab data={tabData} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
+
+export default ApplicationDetailsScreen;
