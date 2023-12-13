3cb17fcf9e54b435e781852cf12e0a508aef0f52
hyobicho <hyobicho@student.42seoul.kr>
2023-12-13 19:22:23 +0900

    [Chore] [FE-31] 주석 삭제 및 색상 정리

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -8,20 +8,20 @@ const Colors = {
   // second
   second: '#EEF8FE',
   // font
-  fontGray01: '#161616', // fontGray01 (ok)
-  fontGray02: '#1E1E20', // fontGray02 (ok)
-  fontGray03: '#4A4A4A', // fontGray03 (ok)
-  fontGray04: '#727272', // fontGray04 (ok)
-  fontGray05: '#8E8E8E', // fontGray05 (ok)
-  fontGray06: '#C1C1C1', // fontGray06 (ok)
-  fontGray07: '#B9B9B9', // fontGray07 (ok)
-  fontGray08: '#F2F2F2', // fontGray08 (ok)
+  fontGray01: '#161616',
+  fontGray02: '#1E1E20',
+  fontGray03: '#4A4A4A',
+  fontGray04: '#727272',
+  fontGray05: '#8E8E8E',
+  fontGray06: '#C1C1C1',
+  fontGray07: '#B9B9B9',
+  fontGray08: '#F2F2F2',
   // line
-  // lineGray01: '#4A4A4A', // lineGray01
-  // lineGray02: '#E3E3E3', // lineGray02
-  lineGray03: '#EEEEEE', // lineGray03 (ok)
-  lineGray04: '#ECECEC', // lineGray04 (ok)
-  lineGray05: '#F5F5F5', // lineGray05 (ok)
+  // lineGray01: '#4A4A4A',
+  // lineGray02: '#E3E3E3',
+  lineGray03: '#EEEEEE',
+  lineGray04: '#ECECEC',
+  lineGray05: '#F5F5F5',
   // etc.
   likeBlank: '#CBCBCBE3',
   footerLight: '#808080',
