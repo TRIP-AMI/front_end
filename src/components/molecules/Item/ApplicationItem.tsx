1daae9bdf969c6eae3c0721003b25f725fe1615e
Myoung Jun Ko <kmj951015@gmail.com>
2023-12-16 22:41:39 +0900

    [Feat] [FE-16] Application Detail 페이지에서 이미지 혹은 제목을 눌렀을 때 동작 처리

--- src/components/molecules/Item/ApplicationItem.tsx
+++ src/components/molecules/Item/ApplicationItem.tsx
@@ -4,6 +4,8 @@ import Colors from '@styles/colors';
 import BlackBadge from '@components/atoms/Tag/BlackBadge';
 import { useSetRecoilState } from 'recoil';
 import modalState from '@utils/recoil/modal';
+import { useNavigation } from '@react-navigation/native';
+import { RootStackNavigationProp } from '@/types/NavigationTypes';
 
 function ApplicationItem({
   imgUrl,
@@ -15,6 +17,7 @@ function ApplicationItem({
   date: string;
 }) {
   const setModal = useSetRecoilState(modalState);
+  const navigation = useNavigation<RootStackNavigationProp>();
 
   const onReviewPress = () => {
     setModal({
@@ -22,11 +25,17 @@ function ApplicationItem({
       applicationItem: { imgUrl, title },
     });
   };
+
+  // TODO 알맞는 컨텐츠로 이동해야 함
   return (
     <View style={styles.container}>
-      <Image resizeMode='cover' style={styles.img} source={{ uri: imgUrl }} />
+      <Pressable onPress={() => navigation.navigate('Content')}>
+        <Image resizeMode='cover' style={styles.img} source={{ uri: imgUrl }} />
+      </Pressable>
       <View style={styles.subContainer}>
-        <Text style={styles.title}>{title}</Text>
+        <Pressable onPress={() => console.log('예약 페이지로 이동 예정')}>
+          <Text style={styles.title}>{title}</Text>
+        </Pressable>
         <Text style={styles.date}>{date}</Text>
         <Pressable style={styles.badge} onPress={onReviewPress}>
           <BlackBadge text='Review' />
