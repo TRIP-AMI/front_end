f13a417fe96aee261738ef680c62ea82f3efe737
hyobicho <hyobicho@student.42seoul.kr>
2023-12-17 13:48:16 +0900

    [Feat] [FE-33] modalHook 기존 코드 살림

--- src/hooks/modalHook.ts
+++ src/hooks/modalHook.ts
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import modalState from '@/utils/recoil/modal';
 import { ModalName } from '@/types/ModalTypes';
 
 const useModalHook = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
   const [modal, setModal] = useRecoilState(modalState);
+
+  const onOpen = () => {
+    setIsVisible(true);
+  };
+
+  const onClose = () => {
+    setIsVisible(false);
+  };
 
   const resetModal = () => {
     setModal(null);
@@ -15,7 +25,15 @@ const useModalHook = () => {
     });
   };
 
-  return { modal, setModal, resetModal, setModalName };
+  return {
+    isVisible,
+    onOpen,
+    onClose,
+    modal,
+    setModal,
+    resetModal,
+    setModalName,
+  };
 };
 
 export default useModalHook;
