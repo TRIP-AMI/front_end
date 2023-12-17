dffc4829b5544a1f5d6cd702ed659aeebb30be3d
hyobicho <hyobicho@student.42seoul.kr>
2023-12-18 04:04:55 +0900

    [Feat] [FE-33] email, pw input error 처리 추가

--- src/components/molecules/Input/LoginInput.tsx
+++ src/components/molecules/Input/LoginInput.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from 'react';
-import { StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import BasicInput from '@/components/atoms/Input/BasicInput';
 import Colors from '@/styles/colors';
 
@@ -7,23 +7,30 @@ export function EmailInput({
   email,
   setEmail,
   placeholder,
+  error,
+  errorText,
 }: {
   email: string;
   setEmail: Dispatch<SetStateAction<string>>;
   placeholder?: string;
+  error?: boolean;
+  errorText?: string;
 }) {
   return (
-    <BasicInput
-      style={styles.input}
-      value={email}
-      autoComplete='username'
-      onChangeText={setEmail}
-      placeholder={placeholder || 'E-mail'}
-      placeholderTextColor={Colors.fontGray05}
-      autoCorrect={false}
-      autoCapitalize='none'
-      autoFocus
-    />
+    <View style={styles.container}>
+      <BasicInput
+        style={styles.input}
+        value={email}
+        autoComplete='username'
+        onChangeText={setEmail}
+        placeholder={placeholder || 'E-mail'}
+        placeholderTextColor={Colors.fontGray05}
+        autoCorrect={false}
+        autoCapitalize='none'
+        autoFocus
+      />
+      {error && <Text style={styles.error}>{errorText}</Text>}
+    </View>
   );
 }
 
@@ -31,30 +38,66 @@ export function PasswordInput({
   password,
   setPassword,
   placeholder,
+  autoFocus,
+  error,
+  errorText,
+  vaildText,
 }: {
   password: string;
   setPassword: Dispatch<SetStateAction<string>>;
   placeholder?: string;
+  autoFocus?: boolean;
+  error?: boolean;
+  errorText?: string;
+  vaildText?: string;
 }) {
   return (
-    <BasicInput
-      style={styles.input}
-      autoComplete='password'
-      value={password}
-      onChangeText={setPassword}
-      placeholder={placeholder || 'Password'}
-      placeholderTextColor={Colors.fontGray05}
-      secureTextEntry
-      maxLength={20}
-    />
+    <View style={styles.container}>
+      <BasicInput
+        style={styles.input}
+        autoComplete='password'
+        value={password}
+        onChangeText={setPassword}
+        placeholder={placeholder || 'Password'}
+        placeholderTextColor={Colors.fontGray05}
+        secureTextEntry
+        maxLength={20}
+        autoFocus={autoFocus}
+      />
+      {error ? (
+        <Text style={styles.error}>{errorText}</Text>
+      ) : (
+        vaildText && <Text style={styles.valid}>{vaildText}</Text>
+      )}
+    </View>
   );
 }
 
+// TODO: 스타일 수정
 const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
   input: {
     fontSize: 14,
     fontFamily: 'Montserrat-Regular',
     lineHeight: 18,
     letterSpacing: -0.28,
+  },
+  error: {
+    color: '#F22222',
+    fontSize: 12,
+    fontFamily: 'Montserrat-Regular',
+    lineHeight: 18,
+    letterSpacing: -0.28,
+    paddingHorizontal: 5,
+  },
+  valid: {
+    color: 'skyblue',
+    fontSize: 12,
+    fontFamily: 'Montserrat-Regular',
+    lineHeight: 18,
+    letterSpacing: -0.28,
+    paddingHorizontal: 5,
   },
 });
