a0f405626f825e46d6d25eb38044507ca21952cb
Myoung Jun Ko <kmj951015@gmail.com>
2023-12-19 01:51:30 +0900

    [Feat] [FE-16] 위쪽 문구 추가

--- src/components/molecules/Item/ApplicationItem.tsx
+++ src/components/molecules/Item/ApplicationItem.tsx
@@ -2,24 +2,31 @@ import ApplicationContentItem from '@components/atoms/Item/ApplicationContentIte
 import { StyleSheet, View } from 'react-native';
 import ApplicationCompleteText from '@components/atoms/Text/ApplicationCompleteText';
 import Spacing from '@styles/spacing';
+import ApplicationDateText from '@components/atoms/Text/ApplicationDateText';
+import ApplicationTable from '@components/molecules/Text/ApplicationTable';
 
 type ApplicationItemProps = {
   imgUrl: string;
   title: string;
   subTitle: string;
   price: string;
+  date: string;
 };
 
 function ApplicationItem({ item }: { item: ApplicationItemProps }) {
   return (
     <View style={styles.container}>
-      <ApplicationCompleteText />
+      <View style={styles.subContainer}>
+        <ApplicationCompleteText />
+        <ApplicationDateText date={item.date} />
+      </View>
       <ApplicationContentItem
         imgUrl={item.imgUrl}
         title={item.title}
         subTitle={item.subTitle}
         price={item.price}
       />
+      <ApplicationTable />
     </View>
   );
 }
@@ -27,6 +34,11 @@ function ApplicationItem({ item }: { item: ApplicationItemProps }) {
 const styles = StyleSheet.create({
   container: {
     marginHorizontal: Spacing.IOS392Margin,
+  },
+  subContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 15,
   },
 });
 
