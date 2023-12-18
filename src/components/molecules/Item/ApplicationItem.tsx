a55354f653e93c19e53405ce5ff4289d7251fecc
Myoung Jun Ko <kmj951015@gmail.com>
2023-12-18 21:46:52 +0900

    [Feat] [FE-16] 기존 구문 ApplicationContentItem 컴포넌트로 분리

--- src/components/molecules/Item/ApplicationItem.tsx
+++ src/components/molecules/Item/ApplicationItem.tsx
@@ -1,85 +1,32 @@
-import { StyleSheet, View, Image, Text, Pressable } from 'react-native';
+import ApplicationContentItem from '@components/atoms/Item/ApplicationContentItem';
+import { StyleSheet, View } from 'react-native';
+import ApplicationCompleteText from '@components/atoms/Text/ApplicationCompleteText';
 import Spacing from '@styles/spacing';
-import Colors from '@styles/colors';
-import BlackBadge from '@components/atoms/Tag/BlackBadge';
-import { useSetRecoilState } from 'recoil';
-import modalState from '@utils/recoil/modal';
-import { useNavigation } from '@react-navigation/native';
-import { RootStackNavigationProp } from '@/types/NavigationTypes';
 
-function ApplicationItem({
-  imgUrl,
-  title,
-  date,
-}: {
+type ApplicationItemProps = {
   imgUrl: string;
   title: string;
-  date: string;
-}) {
-  const setModal = useSetRecoilState(modalState);
-  const navigation = useNavigation<RootStackNavigationProp>();
+  subTitle: string;
+  price: string;
+};
 
-  const onReviewPress = () => {
-    setModal({
-      modalName: 'REVIEW',
-      applicationItem: { imgUrl, title },
-    });
-  };
-
-  // TODO 알맞는 컨텐츠로 이동해야 함
+function ApplicationItem({ item }: { item: ApplicationItemProps }) {
   return (
     <View style={styles.container}>
-      <Pressable onPress={() => navigation.navigate('Content')}>
-        <Image resizeMode='cover' style={styles.img} source={{ uri: imgUrl }} />
-      </Pressable>
-      <View style={styles.subContainer}>
-        <Pressable onPress={() => console.log('예약 페이지로 이동 예정')}>
-          <Text style={styles.title}>{title}</Text>
-        </Pressable>
-        <Text style={styles.date}>{date}</Text>
-        <Pressable style={styles.badge} onPress={onReviewPress}>
-          <BlackBadge text='Review' />
-        </Pressable>
-      </View>
+      <ApplicationCompleteText />
+      <ApplicationContentItem
+        imgUrl={item.imgUrl}
+        title={item.title}
+        subTitle={item.subTitle}
+        price={item.price}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'white',
     marginHorizontal: Spacing.IOS392Margin,
-  },
-  img: {
-    width: 104,
-    height: 100,
-    borderRadius: 5,
-  },
-  subContainer: {
-    flex: 1,
-    marginLeft: 15,
-  },
-  title: {
-    fontSize: 16,
-    fontFamily: 'Montserrat-SemiBold',
-    color: '#000000',
-    letterSpacing: -0.32,
-    lineHeight: 19,
-    marginVertical: 5,
-  },
-  date: {
-    fontSize: 12,
-    fontFamily: 'Montserrat-Regular',
-    color: Colors.fontGray07,
-    letterSpacing: -0.24,
-    lineHeight: 15,
-    marginBottom: 35,
-  },
-  badge: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
   },
 });
 
