42e4bc8ae13fab31f305726c931b55cfc144caba
Myoung Jun Ko <kmj951015@gmail.com>
2023-12-19 14:37:01 +0900

    [Feat] [FE-16] user email 추가

--- src/utils/recoil/user.tsx
+++ src/utils/recoil/user.tsx
@@ -6,6 +6,7 @@ const userState = atom({
     id: '1',
     name: 'Jenny',
     isAmi: true,
+    email: 'tripami@tripami.co.kr',
     profileImgUrl:
       'https://geographical.co.uk/wp-content/uploads/panda1200-1.jpg',
   },
