6ab77239476270731cefca408bc66aae8c858e48
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-12-19 18:58:58 +0900

    [Feat] [FE-35] 업로드 스크린 레이아웃 작업

--- src/screens/UploadScreen.tsx
+++ src/screens/UploadScreen.tsx
@@ -1,8 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Button,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Spacing from '@/styles/spacing';
 import { RootStackNavigationProp } from '@/types/NavigationTypes';
+import SectionDividerBar from '@/components/atoms/etc/SectionDividerBar';
+import BasicInput from '@/components/atoms/Input/BasicInput';
+import PressHashTag from '@/components/atoms/Tag/PressHashTag';
+import ContentInputWrap from '@/components/molecules/Input/ContentInputWrap';
+import Counter from '@/components/molecules/Controller/Counter';
+
+const CategoryList = [
+  { id: '1', label: 'Activity' },
+  { id: '2', label: 'Restaurant' },
+  { id: '3', label: 'Tourism' },
+  { id: '4', label: 'Photo' },
+  { id: '5', label: 'K-POP' },
+  { id: '6', label: 'Shopping' },
+];
 
 export default function UploadScreen() {
   const navigation = useNavigation<RootStackNavigationProp>();
@@ -12,20 +32,112 @@ export default function UploadScreen() {
   };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
       <StatusBar style='auto' />
-      <View>
-        <Text>temp</Text>
-        <Button title='calendar go' onPress={handleCal} />
-      </View>
-    </View>
+
+      {/* content */}
+      <ScrollView style={styles.container}>
+        {/* basic info */}
+        <View
+          style={{
+            paddingHorizontal: Spacing.IOS392Margin,
+          }}
+        >
+          {/* title */}
+          <ContentInputWrap
+            title='Title'
+            content={<BasicInput placeholder='No more than 50 characters.' />}
+          />
+
+          {/* Explanation */}
+          <ContentInputWrap
+            title='Explanation'
+            content={
+              <BasicInput
+                placeholder='No more than 500 characters.'
+                textarea
+                maxLength={500}
+              />
+            }
+          />
+
+          {/* Category */}
+          <ContentInputWrap
+            title='Category'
+            subTitle='Select all relevant categories.'
+            content={
+              <View
+                style={{
+                  flexDirection: 'row',
+                  flexWrap: 'wrap',
+                  width: '80%',
+                }}
+              >
+                {CategoryList.map((ctry) => (
+                  <PressHashTag
+                    key={ctry.id}
+                    label={ctry.label}
+                    style={{ marginRight: 5, marginBottom: 5 }}
+                  />
+                ))}
+              </View>
+            }
+          />
+
+          {/* Hashtag */}
+          <ContentInputWrap
+            title='Hashtag'
+            subTitle='Enter tags associated with the program (Up to 3)'
+            content={<BasicInput placeholder='#tag' />}
+          />
+
+          {/* Location */}
+          <ContentInputWrap
+            title='Location'
+            subTitle='EX) In front of Lotte Tower in Jamsil-dong, Songpa-gu, Seoul'
+            content={<BasicInput placeholder='Please enter a location.' />}
+          />
+
+          {/* Available Dates */}
+          <ContentInputWrap
+            title='Available Dates'
+            content={<Button title='calendar go' onPress={handleCal} />}
+          />
+
+          {/* Time to meet */}
+          <ContentInputWrap
+            title='Time to meet'
+            content={<Button title='calendar go' onPress={handleCal} />}
+          />
+
+          {/* Recruited Persons */}
+          <ContentInputWrap
+            title='Recruited Persons'
+            content={<Counter min={1} max={10} />}
+          />
+
+          {/* E-mail */}
+          <ContentInputWrap title='E-mail' content={<View />} />
+        </View>
+
+        {/* Program Course */}
+        <SectionDividerBar />
+        <View style={{ paddingHorizontal: Spacing.IOS392Margin }}>
+          {/* temp */}
+        </View>
+        {/* Program Cost */}
+        <SectionDividerBar />
+        <View style={{ paddingHorizontal: Spacing.IOS392Margin }}>
+          {/* temp */}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    paddingTop: 15,
     backgroundColor: '#ffffff',
-    paddingHorizontal: Spacing.IOS392Margin,
   },
 });
