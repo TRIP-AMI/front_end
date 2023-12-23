45d5ca732bbc6b58a707f3541e1981d4ec5d8e28
hyobicho <hyobicho@student.42seoul.kr>
2023-12-23 21:54:36 +0900

    [Feat] [FE-34] 유저, 프로필 타입 수정

--- src/types/UserTypes.ts
+++ src/types/UserTypes.ts
@@ -1,5 +1,11 @@
 export type User = {
-  username: string;
+  memberId: number;
+  email: string;
+  nickname: string;
+  imgUrl: string;
+  roles: string[];
+  agreedMarketing: boolean;
+  token: string;
 };
 
-export type Profile = 'AMI' | 'Traveler';
+export type Profile = 'AMI' | 'Tourist';
