3cd01d6799d8b41ec9465b72b042bbaa02ce1a9b
Myoung Jun Ko <kmj951015@gmail.com>
2023-12-27 05:48:51 +0900

    [Feat] [FE-37] ami를 위한 application details 화면 구현

--- src/screens/ApplicationDetailsScreen.tsx
+++ src/screens/ApplicationDetailsScreen.tsx
@@ -2,13 +2,22 @@ import { SafeAreaView, StyleSheet } from 'react-native';
 import BasicTab from '@components/organisms/Section/BasicTab';
 import ApplicationItemList from '@components/organisms/Section/ApplicationItemList';
 import CancellationItemList from '@components/organisms/Section/CancellationItemList';
+import { useRecoilValue } from 'recoil';
+import profileType from '@utils/recoil/profile';
+import ApplicationItemListForAmi from '@components/organisms/Section/ApplicationItemListForAmi';
 
 function ApplicationDetailsScreen() {
+  const profile = useRecoilValue(profileType);
   const tabData = [
     {
       id: 1,
       tabName: 'Application',
-      tabComponent: <ApplicationItemList />,
+      tabComponent:
+        profile === 'AMI' ? (
+          <ApplicationItemListForAmi />
+        ) : (
+          <ApplicationItemList />
+        ),
     },
     {
       id: 2,
@@ -19,7 +28,7 @@ function ApplicationDetailsScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <BasicTab data={tabData} />
+      <BasicTab data={tabData} fullScreen={profile === 'AMI'} />
     </SafeAreaView>
   );
 }
