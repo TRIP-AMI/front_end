4f8027b4765fc967efdacd98dade3455e246947e
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-12-30 23:23:56 +0900

    [Feat] [FE-46] about 완료

--- src/screens/bottomNav/Menu/About/index.tsx
+++ src/screens/bottomNav/Menu/About/index.tsx
@@ -1,9 +1,16 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView, Image } from 'react-native';
+
+// import ContentImg from '../../../../../assets/images/about_tripami.png';
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to CultureX!</Text>
+      <ScrollView>
+        <Image
+          source={require('../../../../../assets/images/about_tripami.png')}
+          style={{ width: '100%', objectFit: 'contain' }}
+        />
+      </ScrollView>
     </View>
   );
 }
