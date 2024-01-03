ad5578f9a475aa9f91e7a9b47afd705d2310481d
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-01-03 17:30:57 +0900

    [Feat] [FE-51] 알림 화면 구현

--- src/components/molecules/Item/NotifactionItem.tsx
+++ src/components/molecules/Item/NotifactionItem.tsx
@@ -1,12 +1,44 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { Octicons } from '@expo/vector-icons';
+import dayjs from 'dayjs';
 import Alarm from '@/types/Notifcation';
 
-export default function NotifactionItem({ alarm }: { alarm: Alarm }) {
+export default function NotifactionItem({
+  alarm,
+  edit,
+  deleteAlarm,
+}: {
+  alarm: Alarm;
+  edit: boolean;
+  deleteAlarm: (id: string) => void;
+}) {
+  const dateFormat = dayjs(alarm.alarmDate).format('HH:ss MM.DD');
+
   return (
     <View style={styles.container}>
-      <View style={{ opacity: alarm.alarmReadState ? 0.3 : 1 }}>
-        <Text style={styles.title}>{alarm.alarmTitle}</Text>
-        <Text style={styles.content}>{alarm.alarmContent}</Text>
+      {/* left */}
+      {edit && (
+        <View style={{ paddingTop: 1, paddingRight: 20 }}>
+          <Pressable
+            onPress={() => {
+              deleteAlarm(alarm.alarmId);
+            }}
+          >
+            <Octicons name='x-circle-fill' size={18} color='#ccc' />
+          </Pressable>
+        </View>
+      )}
+      {/* right */}
+      <View style={[styles.right]}>
+        <View
+          style={[{ flex: 8 }, { opacity: alarm.alarmReadState ? 0.3 : 1 }]}
+        >
+          <Text style={styles.title}>{alarm.alarmTitle}</Text>
+          <Text style={styles.content}>{alarm.alarmContent}</Text>
+        </View>
+        <View style={{ flex: 2, flexDirection: 'row-reverse' }}>
+          <Text style={styles.timeText}>{dateFormat}</Text>
+        </View>
       </View>
     </View>
   );
@@ -14,7 +46,14 @@ export default function NotifactionItem({ alarm }: { alarm: Alarm }) {
 
 const styles = StyleSheet.create({
   container: {
-    paddingVertical: 15,
+    flexDirection: 'row',
+    paddingTop: 15,
+  },
+
+  right: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingBottom: 15,
     borderBottomWidth: 1,
     borderColor: '#ECECEC',
   },
@@ -29,5 +68,12 @@ const styles = StyleSheet.create({
     fontFamily: 'Montserrat-Regular',
     fontSize: 14,
     lineHeight: 15,
+  },
+  timeText: {
+    color: '#B9B9B9',
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 12,
+    lineHeight: 15,
+    letterSpacing: -0.24,
   },
 });
--- src/screens/Notification/index.tsx
+++ src/screens/Notification/index.tsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Modal,
+  Pressable,
+} from 'react-native';
 import NotifactionItem from '@/components/molecules/Item/NotifactionItem';
 import Alarm from '@/types/Notifcation';
 import notificationApi from '@/services/module/notification/notification';
 import { NotificationList } from '@/services/module/notification/response';
 import BlackBadge from '@/components/atoms/Tag/BlackBadge';
 import Spacing from '@/styles/spacing';
+import useModalHook from '@/hooks/modalHook';
+import { NotificationEditOption } from '@/types/Literal/notification';
 
 const processData = (list: NotificationList): Alarm[] => {
   return list.map((item) => {
@@ -19,30 +28,154 @@ const processData = (list: NotificationList): Alarm[] => {
 
 export default function NotificationScreen() {
   const [alarmList, serAlarmList] = useState<Alarm[]>([]);
+  const { isVisible, onOpen, onClose } = useModalHook();
+  const [isEdit, setIsEdit] = useState(false);
+  const badgeText = isEdit ? 'Completion' : 'Edit';
+  const emptyList = alarmList.length === 0;
 
-  const deatSet = async () => {
+  const selectOption: {
+    id: NotificationEditOption;
+    label: string;
+    handler: () => void;
+  }[] = [
+    {
+      id: 'READ_ALL',
+      label: 'Read it all',
+      handler: () => {},
+    },
+    {
+      id: 'DELETE_ALL',
+      label: 'Delete the whole thing',
+      handler: () => {},
+    },
+    {
+      id: 'DELETE_SELECTION',
+      label: 'Delete Selection',
+      handler: () => {
+        onClose();
+        setIsEdit(true);
+      },
+    },
+  ];
+
+  const detaSet = async () => {
     const res = await notificationApi.getNotificationList();
     serAlarmList(processData(res));
   };
 
+  const onEdit = () => {
+    if (isEdit) {
+      setIsEdit(false);
+      return;
+    }
+    onOpen();
+  };
+
+  const deleteAlarm = (id: string) => {
+    const newAlarmList = alarmList.filter((item) => item.alarmId !== id);
+    serAlarmList(newAlarmList);
+  };
+
   useEffect(() => {
-    deatSet();
+    detaSet();
   }, []);
 
   return (
     <View style={styles.container}>
       {/* top */}
       <View style={styles.topInfo}>
-        <Text>{alarmList.length} Total</Text>
-        <BlackBadge text='Edit' />
+        <Text style={styles.totalText}>{alarmList.length} Total</Text>
+        <Pressable onPress={onEdit}>
+          <BlackBadge text={badgeText} />
+        </Pressable>
       </View>
 
+      {/* empty */}
+      {emptyList && (
+        <View
+          style={{
+            paddingTop: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: 'Montserrat-Medium',
+              fontSize: 14,
+              lineHeight: 20,
+              color: '#77787A',
+            }}
+          >
+            There is no notification.
+          </Text>
+        </View>
+      )}
+
       {/* list */}
-      <FlatList
-        data={alarmList}
-        renderItem={(item) => <NotifactionItem alarm={item.item} />}
-        keyExtractor={(item) => item.alarmId}
-      />
+      {!emptyList && (
+        <FlatList
+          data={alarmList}
+          renderItem={(item) => (
+            <NotifactionItem
+              alarm={item.item}
+              edit={isEdit}
+              deleteAlarm={deleteAlarm}
+            />
+          )}
+          keyExtractor={(item) => item.alarmId}
+        />
+      )}
+
+      {/* modal */}
+      <Modal visible={isVisible} animationType='fade' transparent>
+        <Pressable
+          onPress={onClose}
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0.5)',
+          }}
+        >
+          <View style={styles.modalContentWrap}>
+            {/* option */}
+            <View style={{ padding: 20 }}>
+              {selectOption.map((item) => {
+                return (
+                  <Pressable key={item.id} onPress={item.handler}>
+                    <Text
+                      style={{
+                        fontFamily: 'Montserrat-Medium',
+                        fontSize: 14,
+                        lineHeight: 37,
+                      }}
+                    >
+                      {item.label}
+                    </Text>
+                  </Pressable>
+                );
+              })}
+            </View>
+            {/* cancel */}
+            <Pressable
+              onPress={onClose}
+              style={{ padding: 14, borderTopWidth: 1, borderColor: '#EBEBEB' }}
+            >
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  fontFamily: 'Montserrat-SemiBold',
+                  fontSize: 14,
+                  lineHeight: 20,
+                }}
+              >
+                Cancel
+              </Text>
+            </Pressable>
+          </View>
+        </Pressable>
+      </Modal>
     </View>
   );
 }
@@ -58,5 +191,16 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginVertical: 13,
+  },
+  totalText: {
+    color: '#77787A',
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 12,
+    lineHeight: 30,
+  },
+  modalContentWrap: {
+    width: '80%',
+    backgroundColor: '#fff',
+    borderRadius: 5,
   },
 });
