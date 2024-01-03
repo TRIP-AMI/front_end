c973697c1f8472cdb17c8d71dfba06f0aaecbdee
Myoung Jun Ko <kmj951015@gmail.com>
2024-01-03 18:06:02 +0900

    [Refactor] [FE-44] 불필요한 View 제거
    Signed-off-by: Myoung Jun Ko <kmj951015@gmail.com>

--- src/components/molecules/etc/SettingIcon.tsx
+++ src/components/molecules/etc/SettingIcon.tsx
@@ -1,24 +1,22 @@
 import { View, StyleSheet } from 'react-native';
 import { FeatherIconButton } from '@components/atoms/Button/IconButton';
 import { useNavigation } from '@react-navigation/native';
-import { RootStackNavigationProp } from '@/types/NavigationTypes';
+import { MyPageStackNavigationProp } from '@/navigations/MainBottomNavTab/MyPage/MyPageStack';
 
 export default function HomeHeaderIcons() {
-  const navigation = useNavigation<RootStackNavigationProp>();
+  const navigation = useNavigation<MyPageStackNavigationProp>();
   const onOpen = () => {
     navigation.navigate('Setting');
   };
 
   return (
     <View style={styles.container}>
-      <View>
-        <FeatherIconButton
-          icon='settings'
-          size={24}
-          color='#121212'
-          onPress={onOpen}
-        />
-      </View>
+      <FeatherIconButton
+        icon='settings'
+        size={24}
+        color='#121212'
+        onPress={onOpen}
+      />
     </View>
   );
 }
@@ -27,7 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginRight: 26.64,
+    marginHorizontal: 10,
   },
   badge: {
     position: 'absolute',
