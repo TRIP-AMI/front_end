b0b4cc80fabe1287e97b6c47a05b763664accf9a
hyobicho <hyobicho@student.42seoul.kr>
2024-01-03 23:07:16 +0900

    [Feat] [FE-49] 배너 이미지 확대 모달 구현

--- src/types/ModalTypes.ts
+++ src/types/ModalTypes.ts
@@ -6,6 +6,7 @@ export type ModalName =
   | 'JOIN_CANCEL'
   | 'JOIN_COMPLETE'
   | 'AUTH_ALERT'
+  | 'FULL_IMAGE'
   | 'REPORT_COMPLETE'
   | 'APPLY'
   | 'APPLY_CHECK'
@@ -36,6 +37,7 @@ export type CustomModalProps = {
   title?: string;
   applicationItem?: ApplicationItemProps;
   applicationFilter?: ApplicationFilterProps;
+  imgUrl?: string;
 };
 
 export type ModalState = CustomModalProps & {
