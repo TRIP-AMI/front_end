5bc75da9d3ecc30cf185c9c22732b9a89d8682ed
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-01-03 10:29:41 +0900

    [Feat] [FE-48] like item date 추가

--- src/components/molecules/Item/BasicProductItem.tsx
+++ src/components/molecules/Item/BasicProductItem.tsx
@@ -6,13 +6,28 @@ import BasicProductItemSubTitle from '@/components/atoms/Text/BasicProductItemSu
 import BasicProductItemImg from '@/components/atoms/Image/BasicProductItemImg';
 import LikeHeart from '@/components/atoms/Lottie/LikeHeart';
 import Colors from '@/styles/colors';
+import BlackBadge from '@/components/atoms/Tag/BlackBadge';
+
+function LastDateBadge({ date }: { date: Date }) {
+  const now = dayjs();
+
+  if (dayjs(date).diff(now, 'day') !== 0) {
+    return null;
+  }
+
+  return (
+    <View style={{ width: '23%', marginBottom: 5 }}>
+      <BlackBadge text='D-1' />
+    </View>
+  );
+}
 
 export default function BasicProductItem({
   basicItem,
 }: {
   basicItem: BasicItem;
 }) {
-  const dateFormat = dayjs(basicItem.itemDate).format('MM/DD(MMM)');
+  const dateFormat = dayjs(basicItem.itemDate).format('MM/DD(MMM) HH:mm');
   return (
     <View style={styles.basicItemWrap}>
       <View style={styles.imgWrap}>
@@ -21,6 +36,7 @@ export default function BasicProductItem({
         </BasicProductItemImg>
       </View>
       <View>
+        <LastDateBadge date={basicItem.itemDate} />
         <BasicProductItemTitle style={{ marginBottom: 1 }}>
           {basicItem.itemTitle}
         </BasicProductItemTitle>
