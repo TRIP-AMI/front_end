320f00df0e0456d77f3569107d9b2061f5c694e4
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-01-04 13:34:52 +0900

    [Feat] [FE-50] home program api 추가

---
+++ src/services/module/home/index.ts
@@ -0,0 +1,18 @@
+import instance, { BASE_API_URL } from '@/services/config/axios';
+
+export type HomeSectionProgramItem = {
+  id: string;
+  title: string;
+  subTitle: string;
+  imgUrl: string;
+  date: Date;
+};
+export type HomeSectionProgramResponse = HomeSectionProgramItem[];
+
+const getHomeProgramList = (): Promise<HomeSectionProgramResponse> => {
+  const url = `${BASE_API_URL}/homeprogram`;
+  const method = 'GET';
+  return instance({ url, method });
+};
+
+export default { getHomeProgramList };
