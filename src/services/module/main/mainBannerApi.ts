5675c28b04b71abb2b9231aa903b558e0dc696e5
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-01-04 14:43:26 +0900

    [Feat] [FE-50] 배너 로직 수정

--- src/services/module/main/mainBannerApi.ts
+++ src/services/module/main/mainBannerApi.ts
@@ -1,6 +1,14 @@
 import instance, { BASE_API_URL } from '@/services/config/axios';
 
-const getBanner = () => {
+export type BannerListItem = {
+  id: number;
+  link: string;
+  imgUrl: string;
+};
+
+export type BannerListResponse = BannerListItem[];
+
+const getBanner = (): Promise<BannerListResponse> => {
   const url = `${BASE_API_URL}/banner`;
   const method = 'GET';
   return instance({ url, method });
